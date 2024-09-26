60a0174d03112dd77e4ed42fb7162d3ffd2dc175
aufaa03 <142073902+aufaa03@users.noreply.github.com>
2024-09-26 09:05:14 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,43 +1,41 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>PPDB SEKOLAH</title>
+  <link rel="stylesheet" href="style.css">
+  <script src="https://cdn.tailwindcss.com"></script>
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    <title>Hello, world!</title>
-  </head>
-  <body class="">
-    <div class="container text-center border border-dark border-2 rounded-3 d-flex justify-content-center   mt-5">
-        <div class="row">
-            <div class="col">
-                <header>
-
-                    <h2>Penerimaan Peserta Didik Baru</h2>
-                    <h1>SMK PGRI 2 TAMAN</h1>
-                </header>
-                <h4 class="mt-3">Menu</h4>
-                <div>
-                    <ul class="d-flex justify-content-around mt-4">
-                        <li><a href="daftar.php">Formulir Pedaftaran</a></li>
-                        <li><a href="listSiswa.php">Pendaftaran</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
+</head>
+<body>
+  <nav class="bg-transparent top-0 left-0 w-full absolute z-20">
+    <div class="container mx-auto">
+      <img class="drop-shadow-2xl" src="assets/download.png" alt="" width="150px" height="150px" />
     </div>
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    -->
-  </body>
+  </nav>
+  <section class="mt-3">
+    <div class="container relative mx-auto ">
+      <div class="flex flex-wrap">
+       <div class="w-full self-center px-4 lg:w-1/2">
+         <h1 class="text-base font-semibold text-neutral-300 md:text-xl">Selamat datang di Penerimaan Peserta Didik Baru</h1>
+         <h2 class="font-bold text-neutral-100 text-4xl mt-3 lg:text-4xl">SMK PGRI 2 TAMAN</h2>
+         <p class="font-medium text-slate-100 mt-3 mb-10">Bergabunglah bersama kami untuk meraih pendidikan berkualitas dan membangun masa depan yang gemilang!</p>
+         <div class="ml-30">
+           
+          <a class="text-base font-semibold text-dark bg-sky-500 rounded-full hover:shadow-lg hover:opacity:80 transition duration-300 ease-in-out" href="daftar.php">Pendaftaran</a>
+          <a class="text-base font-semibold text-dark bg-sky-500 ml-10 rounded-full hover:shadow-lg hover:opacity:80 transition duration-300 ease-in-out" href="listSiswa.php">Cek Pendaftaran</a>
+         </div>
+       </div>
+      <div class=" backdrop-blur-sm rounded-full bg-white/30 w-full self-end px-4 lg:w-1/2">
+        <div class=" lg:right-0 mt-5">
+          
+        <img class="max-w-full mx-auto rounded-lg drop-shadow-xl" src="assets/20240925_080154.png" alt="" />
+        </div>
+      </div>
+      </div>
+    </div>
+  </section>
+</body>
 </html>
