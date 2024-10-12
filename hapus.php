6e80b377e1df1b949e14c202cf3a83c3a2f11e0e
aufaa03 <142073902+aufaa03@users.noreply.github.com>
2024-10-12 09:30:54 +0700

    Add files via upload

--- hapus.php
+++ hapus.php
@@ -1,3 +1,13 @@
 <?php
 require 'koneksi.php';
+$no_daftar = $_GET['id'];
+$sql = "DELETE FROM siswa WHERE no_daftar ='$no_daftar'";
+$query = $koneksi->query($sql);
+if ($query){
+    echo "<script>alert('no daftar ke $no_daftar berhasil dihapus')</script>";
+    echo"<script> location='listSiswa.php'</script>";
+    exit();
+} else {
+    
+}
 ?>
